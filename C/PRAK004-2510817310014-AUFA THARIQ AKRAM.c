ae36a2778e46c0d3b24fccbab39a53de63cd5614
IntraGH <aufaakram3@gmail.com>
2025-09-28 12:22:03 +0800

    PRAK004-2510817310014-AUFA THARIQ AKRAM.c

---
+++ C/PRAK004-2510817310014-AUFA THARIQ AKRAM.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("#################################################\n");
+    printf("#                                               #\n");
+    printf("#                                               #\n");
+    printf("#              Aufa Thariq Akram                #\n");
+    printf("#                2510817310014                  #\n");
+    printf("#                                               #\n");
+    printf("#                                               #\n");
+    printf("#################################################\n");
+    return 0;
+}
