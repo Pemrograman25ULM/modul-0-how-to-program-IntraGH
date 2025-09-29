5d68b372368736e94524bb8363ce3f9e05a114b8
IntraGH <aufaakram3@gmail.com>
2025-09-29 12:30:08 +0800

    PRAK004-2510817310014-AUFA THARIQ AKRAM.py

--- Python/PRAK004-2510817310014-AUFA THARIQ AKRAM.py
+++ Python/PRAK004-2510817310014-AUFA THARIQ AKRAM.py
@@ -1,8 +1,10 @@
+red = "\033[1;31m"
+reset = "\033[0m"
 print("#################################################")
 print("#                                               #")
 print("#                                               #")
-print("#              Aufa Thariq Akram                #")
-print("#                2510817310014                  #")
+print(f"#              {red}Aufa Thariq Akram{reset}                #")
+print(f"#                {red}2510817310014{reset}                  #")
 print("#                                               #")
 print("#                                               #")
 print("#################################################")
