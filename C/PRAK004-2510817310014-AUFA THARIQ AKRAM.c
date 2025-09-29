ca9c4bd6c0e65b2019c07b2b6a595f32a521e6ee
IntraGH <aufaakram3@gmail.com>
2025-09-29 12:32:27 +0800

    PRAK004-2510817310014-AUFA THARIQ AKRAM.c

--- C/PRAK004-2510817310014-AUFA THARIQ AKRAM.c
+++ C/PRAK004-2510817310014-AUFA THARIQ AKRAM.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
 int main() {
+    char*red = "\033[1;31m";
+    char*reset = "\033[0m";
     printf("#################################################\n");
     printf("#                                               #\n");
     printf("#                                               #\n");
-    printf("#              Aufa Thariq Akram                #\n");
-    printf("#                2510817310014                  #\n");
+    printf("#              %sAufa Thariq Akram%s                #\n",red, reset);
+    printf("#                %s2510817310014%s                  #\n", red, reset);
     printf("#                                               #\n");
     printf("#                                               #\n");
     printf("#################################################\n");
