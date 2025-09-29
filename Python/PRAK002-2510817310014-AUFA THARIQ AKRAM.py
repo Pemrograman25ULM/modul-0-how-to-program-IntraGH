200b1e12bfcbfb546888d7c51ea0e75abfcc4a80
IntraGH <aufaakram3@gmail.com>
2025-09-29 11:46:24 +0800

    PRAK002-2510817310014-AUFA THARIQ AKRAM.py

--- Python/PRAK002-2510817310014-AUFA THARIQ AKRAM.py
+++ Python/PRAK002-2510817310014-AUFA THARIQ AKRAM.py
@@ -1,1 +1,3 @@
-print ("Selamat Pagi, AUFA THARIQ AKRAM \nSelamat Siang, AUFA THARIQ AKRAM\nSelamat Malam, AUFA THARIQ AKRAM")
+red = "\033[1;31m"
+reset = "\033[0m"
+print (f"Selamat Pagi, {red}AUFA THARIQ AKRAM{reset} \nSelamat Siang, {red}AUFA THARIQ AKRAM{reset}\nSelamat Malam, {red}AUFA THARIQ AKRAM{reset}")
